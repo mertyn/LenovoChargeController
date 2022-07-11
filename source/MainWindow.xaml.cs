6bd0f3bc488c399f3fe61763090f649bdade21ff
mertyn <mweber28@hs-mittweida.de>
2022-07-11 11:08:45 +0200

    Replaced window layout; started implementing charge control

--- source/MainWindow.xaml.cs
+++ source/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Reflection;
+using System.Management;
 using System.Windows;
-using System.Windows.Controls;
 using LenovoController.Features;
 
 namespace LenovoController
@@ -12,128 +10,36 @@ namespace LenovoController
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly RadioButton[] _alwaysOnUsbButtons;
-        private readonly AlwaysOnUsbFeature _alwaysOnUsbFeature = new AlwaysOnUsbFeature();
-        private readonly RadioButton[] _batteryButtons;
-        private readonly BatteryFeature _batteryFeature = new BatteryFeature();
-        private readonly RadioButton[] _powerModeButtons;
+
+        private readonly ManagementClass wmi;
         private readonly PowerModeFeature _powerModeFeature = new PowerModeFeature();
-        private readonly FnLockFeature _fnLockFeature = new FnLockFeature();
-        private readonly OverDriveFeature _overDriveFeature = new OverDriveFeature();
-        private readonly TouchpadLockFeature _touchpadLockFeature = new TouchpadLockFeature();
+        private bool _isRunning;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            mainWindow.Title += $" v{AssemblyName.GetAssemblyName(Assembly.GetExecutingAssembly().Location).Version}";
-            _powerModeButtons = new[] {radioQuiet, radioBalance, radioPerformance};
-            _batteryButtons = new[] {radioConservation, radioNormalCharge, radioRapidCharge};
-            _alwaysOnUsbButtons = new[] {radioAlwaysOnUsbOff, radioAlwaysOnUsbOnWhenSleeping, radioAlwaysOnUsbOnAlways};
-            Refresh();
+            // mainWindow.Title += $" v{AssemblyName.GetAssemblyName(Assembly.GetExecutingAssembly().Location).Version}";
+
+            wmi = new ManagementClass("Win32_Battery");
+
         }
 
-        private class FeatureCheck
+        private int GetBatteryPercent()
         {
-            private readonly Action _check;
-            private readonly Action _disable;
+            int percent = -1;
+            foreach (ManagementBaseObject battery in wmi.GetInstances())
+                percent = Convert.ToInt16(battery["EstimatedChargeRemaining"]);
 
-            internal FeatureCheck(Action check, Action disable)
-            {
-                _check = check;
-                _disable = disable;
-            }
-
-            internal void Check() => _check();
-            internal void Disable() => _disable();
+            return percent;
         }
 
-        private void Refresh()
+        private void OnStartStop(object sender, RoutedEventArgs e)
         {
-            var features = new[]
-            {
-                new FeatureCheck(
-                    () => _powerModeButtons[(int) _powerModeFeature.GetState()].IsChecked = true,
-                    () => DisableControls(_powerModeButtons)),
-                new FeatureCheck(
-                    () => _batteryButtons[(int) _batteryFeature.GetState()].IsChecked = true,
-                    () => DisableControls(_batteryButtons)),
-                new FeatureCheck(
-                    () => _alwaysOnUsbButtons[(int) _alwaysOnUsbFeature.GetState()].IsChecked = true,
-                    () => DisableControls(_alwaysOnUsbButtons)),
-                new FeatureCheck(
-                    () => chkOverDrive.IsChecked = _overDriveFeature.GetState() == OverDriveState.On,
-                    () => chkOverDrive.IsEnabled = false),
-                new FeatureCheck(
-                    () => chkTouchpadLock.IsChecked = _touchpadLockFeature.GetState() == TouchpadLockState.On,
-                    () => chkTouchpadLock.IsEnabled = false),
-                new FeatureCheck(
-                    () => chkFnLock.IsChecked = _fnLockFeature.GetState() == FnLockState.On,
-                    () => chkFnLock.IsEnabled = false)
-            };
+            _isRunning = !_isRunning;
 
-            foreach (var feature in features)
-            {
-                try
-                {
-                    feature.Check();
-                }
-                catch (Exception e)
-                {
-                    Trace.TraceInformation("Could not refresh feature: " + e);
-                    feature.Disable();
-                }
-            }
+            StartStop.Content = _isRunning ? "Stop" : "Start";
         }
 
-        private void DisableControls(Control[] buttons)
-        {
-            foreach (var btn in buttons)
-                btn.IsEnabled = false;
-        }
-
-        private void btnRefresh_Click(object sender, RoutedEventArgs e)
-        {
-            Refresh();
-        }
-
-        private void radioPowerMode_Checked(object sender, RoutedEventArgs e)
-        {
-            _powerModeFeature.SetState((PowerModeState) Array.IndexOf(_powerModeButtons, sender));
-        }
-
-        private void radioBattery_Checked(object sender, RoutedEventArgs e)
-        {
-            _batteryFeature.SetState((BatteryState) Array.IndexOf(_batteryButtons, sender));
-        }
-
-        private void radioAlwaysOnUsb_Checked(object sender, RoutedEventArgs e)
-        {
-            _alwaysOnUsbFeature.SetState((AlwaysOnUsbState) Array.IndexOf(_alwaysOnUsbButtons, sender));
-        }
-
-        private void chkOverDrive_Checked(object sender, RoutedEventArgs e)
-        {
-            var state = chkOverDrive.IsChecked.GetValueOrDefault(false)
-                ? OverDriveState.On
-                : OverDriveState.Off;
-            _overDriveFeature.SetState(state);
-        }
-
-        private void chkTouchpadLock_Checked(object sender, RoutedEventArgs e)
-        {
-            var state = chkTouchpadLock.IsChecked.GetValueOrDefault(false)
-                ? TouchpadLockState.On
-                : TouchpadLockState.Off;
-            _touchpadLockFeature.SetState(state);
-        }
-
-        private void chkFnLock_Checked(object sender, RoutedEventArgs e)
-        {
-            var state = chkFnLock.IsChecked.GetValueOrDefault(false)
-                ? FnLockState.On
-                : FnLockState.Off;
-            _fnLockFeature.SetState(state);
-        }
     }
 }
